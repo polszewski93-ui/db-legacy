6be3a7f8724f42c4bdefe7ad536f62653923ede8
polszewski93-ui <polszewski93@gmail.com>
2026-05-15 15:25:53 +0000

    fix: proxy_client results.endpoint() -> results.begin()->endpoint()

--- src/framework/proxy/proxy_client.cpp
+++ src/framework/proxy/proxy_client.cpp
@@ -106,7 +106,7 @@ void Proxy::connect()
             }
             endpoint = boost::asio::ip::tcp::endpoint(address, self->m_port);
         } else {
-            endpoint = results.endpoint();
+            endpoint = results.begin()->endpoint();
             endpoint.port(self->m_port);
         }
         self->m_resolvedIp = endpoint.address().to_string();
